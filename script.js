31ddcbffa69f9fc0fd763a93239b43bddab46fdc
MarilouAiko <e1082409@cmaisonneuve.qc.ca>
2025-11-09 18:49:57 -0500

    Ajout de la section stories (ajouts css et script.js)

---
+++ script.js
@@ -0,0 +1,28 @@
+// Chargement d'images depuis l'API Pexels
+const API_KEY = "YEmthrJDjy7vr7tybv61l9DIASRDocDqYjI7oGn28VdSMphODN3AXMXH"; // Clé API Pexels (obligatoire pour obtenir leurs images et vidéos)
+
+// mieux avec try catch pour gérer les erreurs sinon ça plante
+const LoadImage = async () => { // Fonction asynchrone pour charger des images depuis Pexels
+    try {
+        const response = await fetch("https://api.pexels.com/v1/curated?per_page=10", { // Requête à l'API Pexels pour obtenir des images (await sert à attendre la réponse de fetch (servant à faire des requêtes HTTP)
+            headers: { // headers indique les informations supplémentaires envoyées avec la requête HTTP
+                Authorization: API_KEY // Authentification avec la clé API qui est obligatoire pour accéder aux ressources de l'API
+            }
+        });
+        
+        const data = await response.json(); // Conversion de la réponse en JSON (la méthode json() lit le corps de la réponse et le convertit en un objet JavaScript)
+        
+        // Affiche plusieurs images dans le conteneur
+        data.photos.forEach(photo => { // Parcourt chaque photo dans les données reçues
+            const img = document.createElement("img"); // Crée un élément image HTML
+            img.src = photo.src.medium; // Utilise l'URL de l'image en taille moyenne
+            img.alt = photo.alt || "Image de l'utilisateur"; // Texte alternatif
+            img.classList.add("story-image"); // Ajoute une classe CSS à l'image
+            document.querySelector(".stories-container").appendChild(img); // Ajoute l'image au conteneur
+        });
+    } catch (error) {
+        console.error(error); // Affiche l'erreur dans la console
+    }
+};
+
+window.onload = LoadImage; // Appelle la fonction LoadImage lorsque la fenêtre est chargée
