570894ea290035db628bbc8922d453fe2fb21eac
Cosmos-Prince <alek.hanachian@gmail.com>
2025-12-01 10:48:02 -0500

    toujours plus de work!
    manque juste le backend coté serveur et finir la fonction lors du click du boutton de post

--- scriptPost.js
+++ scriptPost.js
@@ -1,4 +1,18 @@
-document.getElementById('fichier').onchange = function () {
+const bouttonPost = document.getElementById("button-formulaire-post")
+const img = document.getElementById('image')
+const description = document.getElementById("desc")
+const confirmation = document.getElementById("confirmationPost")
+var fichier = document.getElementById('fichier')
+
+fichier.onchange = function () {
   var src = URL.createObjectURL(this.files[0])
-  document.getElementById('image').src = src
+  img.src = src
 }
+
+bouttonPost.addEventListener('click', (event) => {
+  event.preventDefault() 
+  if (fichier.files.length == 0) {
+    confirmation.classList.add("error")
+    confirmation.textContent = "Veuillez choisir une image pour la publication"
+  }
+})
