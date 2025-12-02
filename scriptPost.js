b25dabb73fee01581f34f268a6cb42df166b418a
Alek Hanachian <alek.hanachian@gmail.com>
2025-12-02 12:31:43 -0500

    fix boutton pour nouveau post mais peux pas envoyer d'images

--- scriptPost.js
+++ scriptPost.js
@@ -9,29 +9,49 @@ fichier.onchange = function () {
   img.src = src
 }
 
+//transformer l'image en base64 pour l'envoi en json
+function toBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = reject
+  })
+}
+
 bouttonPost.addEventListener('click', async (event) => {
   event.preventDefault()
   if (fichier.files.length == 0) {
+    confirmation.classList.remove('confirm')
     confirmation.classList.add("error")
     confirmation.textContent = "Veuillez choisir une image pour la publication"
+    return
   }
+  const imgBase64 = await toBase64(fichier.files[0])
 
   //route pour ajouter un post
   const resultat = await fetch("/addPublication", {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      image:fichier,
-      biographie: description 
+      image: imgBase64,
+      biographie: description.value
     })
   })
 
-  if (resultat.ok){
-  // reset la page lors de la publication et msg de confirmationn
-  fichier.value = ''
-  img.value = ''
-  confirmation.classList.add('confirm')
-  confirmation.textContent = 'Publication enregistrée avec succès'
+  if (resultat.ok) {
+    // reset la page lors de la publication et msg de confirmationn
+    fichier.value = ''
+    img.src = ''
+    confirmation.classList.remove('error')
+    confirmation.classList.add('confirm')
+    confirmation.textContent = 'Publication enregistrée avec succès'
+  }
+  else {
+    confirmation.classList.remove('confirm')
+    confirmation.classList.add("error")
+    confirmation.textContent = "Erreur lors de la publication"
+    return
   }
 
   console.log('Ta cliqué')
